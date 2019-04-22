1a573f39baf4423c387091fff293fcd95224db63
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-04-22 20:54:23 -0300

    criada funcao para imprimir em ordem alfabetica

--- Library/src/library/Inventory.java
+++ Library/src/library/Inventory.java
@@ -16,7 +16,26 @@ public class Inventory {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Book i1;
+        i1 = new Book("dom quixote", true, "no comment", "miguel de cervantes", 300);
+        Book i2;
+        i2 = new Book("cem anos de solidao", true, "no comment", "gabriel garcia marquez", 500);
+        Book i3;
+        i3 = new Book("orgulho e preconceito", true, "no comment", "jane austen", 415);
+        Book i4;
+        i4 = new Book("harry potter", false, "no comment", "jk rowling", 150);        
+        Book i5;
+        i5 = new Book("o apanhador no campo de centeio", false, "no comment", "jd salinger", 150);
+        
+                      
+        LibraryItems l = new LibraryItems(5);
+        l.Insert(i1);
+        l.Insert(i2);
+        l.Insert(i3);
+        l.Insert(i4);
+        l.Insert(i5);
+         
+        l.printAlphabeticTitle();       
     }
     
 }
--- Library/src/library/LibraryItems.java
+++ Library/src/library/LibraryItems.java
@@ -22,24 +22,24 @@ public class LibraryItems {
     
     public void Insert(Item newItem){
         if(numberOfElements >= items.length){
-            Item[] novaArea = new Book[items.length * 2];
+            Item[] novaArea = new Item[items.length * 2];
             
             if (novaArea != null) 
             {                
-                for (int i = 0; i < numberOfElements; i++)
-                    novaArea[i] = items[i];
+                System.arraycopy(items, 0, novaArea, 0, numberOfElements);
                 
                 items = novaArea;                
             }   
             else
                 System.err.println("Não foi possível inserir o item. Não há memória disponível");
-                
-        items[numberOfElements++] = newItem;}        
+        }
+        
+        items[numberOfElements++] = newItem;        
     }
     
     public void print(){
         for (int i = 0; i < numberOfElements; i++)
-            System.out.println("[ "+i+" ] " + items[i]);
+            System.out.println("[ "+i+" ] " + items[i].toString());
     }
     
     public Item remove(int index){
@@ -80,5 +80,76 @@ public class LibraryItems {
                 list.add(i);
         }          
         return list;       
-    }            
+    }
+    
+    public void printAlphabeticTitle(){
+        //menor_c controla qual palavra vem antes
+        //essa string é incializada em '|' porque esse caractere vem depois de qualquer letra em ASCII
+        String menor_c = "|";
+        int menor_c_index = 0;
+        String s;
+        LibraryItems auxItems;
+        int menor_len;
+        int k;
+        
+        //cria um auxiliar da lista e copia os elementos dela
+        auxItems = new LibraryItems(numberOfElements);
+        auxItems.numberOfElements = numberOfElements;
+        
+        System.arraycopy(items, 0, auxItems.items, 0, numberOfElements); 
+        
+        //esse loop serve para controlar os elementos que serao impressos        
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            //esse loop serve para saber qual item deve ser impresso
+            for (int j = 0; j < auxItems.numberOfElements; j++)
+            {
+                //s vai receber a string atual que será testada
+                s = (auxItems.items[j].getTitle());
+                
+                //caso a primeira letra seja igual
+                if(s.charAt(0) == menor_c.charAt(0)){
+                    
+                    //testa-se qual o menor comprimento de palavra e armazena-o em menor_len
+                    if(s.length() > menor_c.length())
+                        menor_len = menor_c.length();
+                    else menor_len = s.length();
+                    
+                    //compara letra por letra da palavra ate encontrar uma diferente
+                    for(k = 1;k < menor_len; k++) {
+                        if(s.charAt(k) < menor_c.charAt(k)) {
+                            menor_c_index = j;
+                            menor_c = s;
+                            break;
+                        } else {
+                            if(s.charAt(k) > menor_c.charAt(k))
+                                break;
+                        }          
+                    }
+                    
+                    //se k for igual ao comprimento, a menor palavra vem antes(eg: sol e solidao,onde sol vem antes na ordem alfabetica)
+                    if(k == s.length()){
+                        menor_c_index = j;
+                        menor_c = s;
+                    }
+                        
+                }
+                
+                //se a primeira letra da string for menor, ela vem antes
+                if(s.charAt(0) < menor_c.charAt(0)) {
+                    menor_c_index = j;
+                    menor_c = s;
+                }                
+            }
+            System.out.println("[ "+i+" ] " + auxItems.items[menor_c_index].toString());
+            //apos ser impresso, o item é removido da lista auxiliar
+            auxItems.remove(menor_c_index);
+            //essa string recebe '|' porque esse caractere vem depois de qualquer letra em ASCII
+            menor_c = "|";           
+        }
+    }
+    
+    public void printAlphabeticAuthor(){
+        
+    }
 }
